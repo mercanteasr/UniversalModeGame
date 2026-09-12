1f0f52fff1d251a72bd78326a57145d18fc93824
Ricardo Mercante <mercanteasr@gmail.com>
2026-09-12 07:48:14 -0300

    Atualização

--- src/Players.java
+++ src/Players.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Players {
+    Scanner sc = new Scanner(System.in);
+
+    Universal player1 = new Universal();
+    Universal player2 = new Universal();
+
+    // defining the extent of the damage
+    int damageP1 = player1.getStrengthPlayer() / 2;
+    int damageP2 = player2.getStrengthPlayer() / 2;
+
+    // defining defense size
+    double defenseP1 = player1.getDefensePlayer() / 1.3;
+    double defenseP2 = player2.getDefensePlayer() / 1.3;
+
+
+    public void assign(Universal player){
+        // Request name of Champion
+        System.out.println("Champion's name: ");
+        String name = sc.next();
+        player.setNamePlayer(name);
+        // Request strength of Champion
+        System.out.println(name+" Strength: ");
+        int strength = sc.nextInt();
+        player.setStrengthPlayer(strength);
+        // Request Defense of Champion
+        System.out.println(name+" Defense: ");
+        int defense = sc.nextInt();
+        player.setDefensePlayer(defense);
+
+    }
+    public void testechamar(){
+        System.out.println("\nNOME 1..."+player1.getNamePlayer());
+        System.out.println("\nFORÇA 1..."+player1.getStrengthPlayer());
+        System.out.println("\nDEFESA 1..."+player1.getDefensePlayer());
+        System.out.println("__________________________________________");
+        System.out.println("\nNOME 2..."+player2.getNamePlayer());
+        System.out.println("\nFORÇA 2..."+player2.getStrengthPlayer());
+        System.out.println("\nDEFESA 2..."+player2.getDefensePlayer());
+    }
+
+    public void battle(){
+
+    }
+
 }
--- src/Universal.java
+++ src/Universal.java
@@ -1,4 +1,68 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Universal {
+    private String namePlayer = "";
+    private int strengthPlayer = 0;
+    private int defensePlayer = 0;
+
+     static Players players = new Players();
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+
+        boolean opcao = true;
+        boolean description;
+        do {
+            System.out.println("Welcome to UUniversal Mode Game");
+            System.out.println("Want to read our description?");
+
+            description = sc.nextBoolean();
+            if (description == true) {
+
+                System.out.println("This mini-game was developed by Ricardo Augusto as a practical learning project.");
+                System.out.println(" Its main purpose is to apply and consolidate fundamental Java programming concepts");
+                System.out.println("serving as a simple hands-on exercise to continuously improve software development skills.");
+            }
+
+            System.out.println("\n _-_-_ Attributes _-_-_ \n");
+
+            System.out.println("First Champion\n");
+            players.assign(players.player1);
+            System.out.println("Second Champion\n");
+            players.assign(players.player2);
+
+            players.testechamar();
+
+
+
+
+                System.out.println("exit or continue? [true] or [false]");
+                opcao = sc.nextBoolean();
+            }while (opcao != false) ;
+
+    }
+
+    public void setDefensePlayer(int defensePlayer) {
+        this.defensePlayer = defensePlayer;
+    }
+    public void setNamePlayer(String namePlayer){
+        this.namePlayer = namePlayer;
+    }
+    public void setStrengthPlayer(int strengthPlayer){
+        this.strengthPlayer = strengthPlayer;
+    }
+    public int getDefensePlayer(){
+        return defensePlayer;
+    }
+    public int getStrengthPlayer(){
+        return strengthPlayer;
+    }
+    public String getNamePlayer(){
+        return namePlayer;
+    }
+
+
+
 }
